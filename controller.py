ae2239720334a257126cb1249de94495b50cf571
Eric Hennenfent <ehennenfent@imsa.edu>
2017-12-10 08:56:43 -0600

    Fixed stupid bugs in the controller

--- controller.py
+++ controller.py
@@ -20,30 +20,36 @@ incubator = api.get_datasource('5a224554c03f9721f59934ff')
 variables = incubator.get_variables()
 
 def get_variable(label):
+    
     for variable in variables:
         if str(variable.label) == label:
+            print("Found variable %s: %s" % (variable.label, variable.id))
             return variable
     raise Exception("Could not find variable %s!" % label)
-    
+
+
+temperature_1 = get_variable('temperature-1')
+temperature_2 = get_variable('temperature-2')
+humidity_1 = get_variable('humidity-1')
+humidity_2 = get_variable('humidity-2')
+heater_1 = get_variable('heater-1')
+heater_2 = get_variable('heater-2')
+uptime = get_variable('uptime')
+
+last_avg_temp = 0.0
+
 def toggle_heater_state():
+    global HEATER_IS_ON
     if HEATER_IS_ON:
+        print("Turning heater off at {0:0.1f} degrees C".format(last_avg_temp))
         GPIO.output(12, GPIO.LOW)
         GPIO.output(13, GPIO.LOW)
         HEATER_IS_ON = False
     else:
+        print("Turning heater on at {0:0.1f} degrees C".format(last_avg_temp))
         GPIO.output(12, GPIO.HIGH)
         GPIO.output(13, GPIO.HIGH)
         HEATER_IS_ON = True
-
-temperature_1 = api.get_variable('temperature-1')
-temperature_2 = api.get_variable('temperature-2')
-humidity_1 = api.get_variable('humidity-1')
-humidity_2 = api.get_variable('humidity-2')
-heater_1 = api.get_variable('heater-1')
-heater_2 = api.get_variable('heater-2')
-uptime = api.get_variable('uptime')
-
-last_avg_temp = 0.0
 
 def write_sensor_data(temp1, temp2, hum1, hum2, heat1, heat2):
     timestamp = time.time()
@@ -56,11 +62,11 @@ def write_sensor_data(temp1, temp2, hum1, hum2, heat1, heat2):
     heater_2.save_value({'value': heat2, timestamp:timestamp})
     uptime.save_value({'value': 1, timestamp:timestamp})
     
-
+print("Beginning main event loop")
 while(True):
     try:
-        hum_1, temp_1 = Adafruit_DHT.read_retry(AM2302, 14)
-        hum_2, temp_2 = Adafruit_DHT.read_retry(AM2302, 15)
+        hum_1, temp_1 = read_retry(AM2302, 14)
+        hum_2, temp_2 = read_retry(AM2302, 15)
         last_avg_temp = (temp_1 + temp_2) / 2
         
         if HEATER_IS_ON and (last_avg_temp > 30.2):
@@ -74,5 +80,17 @@ while(True):
         
         time.sleep(5)
     except:
-        print("Something terrible happened!")
+        print("Program exiting! Turning heater off...")
+        try:
+            GPIO.output(12, GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
+            HEATER_IS_ON = False
+            print("Turned heater off safely")
+        except:
+            print("Uh oh! Couldn't power off the heater")
+        try:
+            GPIO.cleanup()
+            print("Cleaned up GPIO Pins")
+        except:
+            print("Couldn't clean up GPIO pins!")
         raise
