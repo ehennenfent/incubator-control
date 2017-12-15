8eb3f823acbb9601968a393768f09da455f25f87
Eric Hennenfent <ehennenfent@imsa.edu>
2017-12-15 04:25:25 -0600

    bulk variable upload

--- controller.py
+++ controller.py
@@ -7,6 +7,7 @@ from secrets import master_incubator_token
 from Adafruit_DHT import read_retry, AM2302
 import RPi.GPIO as GPIO
 import time
+import traceback
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)
@@ -20,13 +21,15 @@ incubator = api.get_datasource('5a224554c03f9721f59934ff')
 variables = incubator.get_variables()
 
 def get_variable(label):
-    
+
     for variable in variables:
         if str(variable.label) == label:
             print("Found variable %s: %s" % (variable.label, variable.id))
             return variable
     raise Exception("Could not find variable %s!" % label)
 
+def get_time():
+    return int(time.time() * 1000)
 
 temperature_1 = get_variable('temperature-1')
 temperature_2 = get_variable('temperature-2')
@@ -52,54 +55,38 @@ def toggle_heater_state():
         HEATER_IS_ON = True
 
 def write_sensor_data(temp1, temp2, hum1, hum2, heat1, heat2):
-    timestamp = time.time()
-    
-    try:
-        temperature_1.save_value({'value': temp1, 'timestamp':int(timestamp)})
-    except:
-        print("Error Uploading to ubidots: could not upload temperature_1")
-    try:
-        temperature_2.save_value({'value': temp2, 'timestamp':int(timestamp)})
-    except:
-        print("Error Uploading to ubidots: could not upload temperature_2")
-    try:
-        humidity_1.save_value({'value': hum1, 'timestamp':int(timestamp)})
-    except:
-        print("Error Uploading to ubidots: could not upload humidity_1")
-    try:
-        humidity_2.save_value({'value': hum2, 'timestamp':int(timestamp)})
-    except:
-        print("Error Uploading to ubidots: could not upload humidity_2")
-    try:
-        heater_1.save_value({'value': heat1, 'timestamp':int(timestamp)})
-    except:
-        print("Error Uploading to ubidots: could not upload heater_1")
-    try:
-        heater_2.save_value({'value': heat2, 'timestamp':int(timestamp)})
-    except:
-        print("Error Uploading to ubidots: could not upload heater_2")
-    try:
-        uptime.save_value({'value': 1, 'timestamp':int(timestamp)})
-    except:
-        print("Error Uploading to ubidots: could not upload Uptime")
+    timestamp = get_time()
 
-    
+    try:
+        api.save_collection([
+            {'variable': temperature_1.id, 'value': temp1, timestamp:timestamp},
+            {'variable': temperature_2.id, 'value': temp2, timestamp:timestamp},
+            {'variable': humidity_1.id, 'value': hum1, timestamp:timestamp},
+            {'variable': humidity_2.id, 'value': hum2, timestamp:timestamp},
+            {'variable': heater_1.id, 'value': heat1, timestamp:timestamp},
+            {'variable': heater_2.id, 'value': heat2, timestamp:timestamp},
+            {'variable': uptime.id, 'value': 1, timestamp:timestamp}]
+    except:
+        print("Error saving data to ubidots")
+        traceback.print_exc()
+
+
 print("Beginning main event loop")
 while(True):
     try:
         hum_1, temp_1 = read_retry(AM2302, 14)
         hum_2, temp_2 = read_retry(AM2302, 15)
         last_avg_temp = (temp_1 + temp_2) / 2
-        
+
         if HEATER_IS_ON and (last_avg_temp > 30.2):
             toggle_heater_state()
-        
+
         if not HEATER_IS_ON and (last_avg_temp < 29.8):
             toggle_heater_state()
-        
-        hs = 100 if HEATER_IS_ON else 0
+
+        hs = 1 if HEATER_IS_ON else 0
         write_sensor_data(temp_1, temp_2, hum_1, hum_2, hs, hs)
-        
+
         time.sleep(5)
     except:
         print("Program exiting! Turning heater off...")
